18fb3418b5a02b1fa48d8d55ae320997fef40685
Ezabulbary <ezabul.bary@gmail.com>
2022-05-08 22:44:40 +0600

    middel ware install

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send('Look mama! we can write npm.')
-})
+    res.send('Look mama! I can write node')
+});
 
 const users = [
     { id: 1, name: 'abdul alim', job: 'khai shudhu halim' },
@@ -14,19 +18,24 @@ const users = [
     { id: 5, name: 'abdul baten', job: 'khai shudhu halim' },
     { id: 6, name: 'abdul hasan', job: 'khai shudhu halim' },
     { id: 7, name: 'abdul mofiz', job: 'khai shudhu halim' }
-]
+];
 
 app.get('/users', (req, res) => {
     res.send(users)
-})
+});
 
 app.get('/user/:id', (req, res) => {
     console.log(req.params);
     const id = parseInt(req.params.id);
     const user = users.find(u => u.id === id);
     res.send(user)
-})
+});
+
+app.post('/user', (req, res) => {
+    console.log(req)
+    res.send('post method success')
+});
 
 app.listen(port, () => {
     console.log('Listening to port', port)
-})
+});
